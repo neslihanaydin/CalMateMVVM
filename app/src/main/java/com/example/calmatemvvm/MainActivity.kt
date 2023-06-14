1e6a102dc0b8c94db0efd4f0b14cd2943e9c0ea6
Neslihan TURPCU <neslihanaydin43@gmail.com>
2023-06-13 22:55:47 -0700

    AppComponent and AppViewModel created in the MainActivity

--- app/src/main/java/com/example/calmatemvvm/MainActivity.kt
+++ app/src/main/java/com/example/calmatemvvm/MainActivity.kt
@@ -2,10 +2,22 @@ package com.example.calmatemvvm
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.calmatemvvm.app.AppViewModel
+import com.example.calmatemvvm.dagger.CoreModule
+import com.example.calmatemvvm.dagger.DaggerAppComponent
+import com.example.calmatemvvm.dagger.RoomModule
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var appViewModel: AppViewModel
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val appComponent = DaggerAppComponent.builder()
+            .build()
+        val appViewModel = appComponent.getAppViewModel().also {
+            this.appViewModel = it
+        }
     }
 }
